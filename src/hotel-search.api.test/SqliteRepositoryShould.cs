37cbc4802f9ecd9468f74f3019c83c69be8bfd8d
Filip Vukovinski <filip.vukovinski@net.hr>
2024-10-27 11:11:25 +0100

    more tests

--- src/hotel-search.api.test/SqliteRepositoryShould.cs
+++ src/hotel-search.api.test/SqliteRepositoryShould.cs
@@ -1,4 +1,3 @@
-using hotel_search.api;
 namespace hotel_search.api.test;
 
 public class SqliteRepositoryShould
@@ -6,14 +5,33 @@ public class SqliteRepositoryShould
     [Fact]
     public void ContainItemAfterInsert()
     {
-        var repository = new HotelSqliteDatabase("Data Source=HotelSearch;Mode=Memory;Cache=Shared");
+        using var repository = new HotelSqliteDatabase("Data Source=HotelSearch;Mode=Memory;Cache=Shared");
         repository.CreateTables();
 
         repository.InsertHotel(new Hotel { Id = 1, Name = "Sherwood", Price = 55m, LocationLatitude = "54", LocationLongitude = "31" });
         var hotels = repository.GetAll();
 
+        Assert.True(hotels.Count != 0);
+        Assert.True(hotels[0].Id == 1);
+        repository.DeleteTables();
+    }
+
+    [Fact]
+    public void NotContainItemAfterDelete()
+    {
+        using var repository = new HotelSqliteDatabase("Data Source=HotelSearch;Mode=Memory;Cache=Shared");
+        repository.CreateTables();
+
+        repository.InsertHotel(new Hotel { Id = 1, Name = "Sherwood", Price = 55m, LocationLatitude = "54", LocationLongitude = "31" });
+        var hotels = repository.GetAll();
 
         Assert.True(hotels.Any());
         Assert.True(hotels[0].Id == 1);
+
+        repository.DeleteHotel(new Hotel { Id = 1 });
+        hotels = repository.GetAll();
+        
+        Assert.True(hotels.Count == 0);
+        repository.DeleteTables();
     }
 }
