19615341f9512dbd7ea13134c9999519c0c9edb5
Filip Vukovinski <filip.vukovinski@net.hr>
2024-10-27 09:46:56 +0100

    removed unicode char

--- src/hotel-search.api/Service/HotelSearchService.cs
+++ src/hotel-search.api/Service/HotelSearchService.cs
@@ -47,7 +47,7 @@
             // https://stackoverflow.com/questions/6366408/calculating-distance-between-two-latitude-and-longitude-geocoordinates
             double rad(double angle) => angle * 0.017453292519943295769236907684886127d; // = angle * Math.Pi / 180.0d
             double havf(double diff) => Math.Pow(Math.Sin(rad(diff) / 2d), 2); // = sin²(diff / 2)
-            return 12745.6 * Math.Asin(Math.Sqrt(havf(lat2 - lat1) + Math.Cos(rad(lat1)) * Math.Cos(rad(lat2)) * havf(lon2 - lon1))); // earth radius 6.372,8‬km x 2 = 12745.6
+            return 12745.6 * Math.Asin(Math.Sqrt(havf(lat2 - lat1) + Math.Cos(rad(lat1)) * Math.Cos(rad(lat2)) * havf(lon2 - lon1))); // earth radius 6.372,8 km x 2 = 12745.6
         }
 
         public static double CalculateMeters(double lat1, double lon1, double lat2, double lon2)
